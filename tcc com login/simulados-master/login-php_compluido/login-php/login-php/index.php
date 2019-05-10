f27035003e647b5161b1500e13cf4bb434567542
Rulian Carlo Farias <36968039+Rulian-Farias@users.noreply.github.com>
2019-05-10 14:46:55 -0300

    update3

---
+++ tcc com login/simulados-master/login-php_compluido/login-php/login-php/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+
+	<link rel="stylesheet" type="text/css" href="css.css">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+
+	<title></title>
+</head>
+<body>
+  
+
+	<?php require_once 'menu.php'; ?>
+  <img src="iii.png" width="100%" height="900px">
+
+ 
+
+
+<br><br><br></br>
+<div class="container">
+  <div class="row">
+
+
+<div class="col-sm">
+   <img src="img/noticias.png" width="50%"><font size="5" color=" red"> Notícias</font>
+</a>
+    </div>
+
+    <div class="col-sm">
+   <a href="https://glo.bo/2WaBhuW"> <img src="img/aluno_enem.jpg" width="30%" > 
+    Candidato que não conseguiu isenção na taxa do Enem 2019 pode entrar 
+    com recurso até esta sexta.
+   </a>
+    </div>
+
+    <div class="col-sm">
+      <a href="https://bit.ly/2vpmfp7"> <img src="img/aluno_enem.jpg" width="30%" >
+    Estudante com isenção negada no Enem ainda pode entrar com recurso.
+</a>
+    </div>
+    <div class="col-sm">
+  <a href="https://enem2019.pro/"><img src="img/aluno_enem.jpg" width="30%" >  
+    Apostila Enem 2019
+</a>
+    </div>
+  </div>
+</div>
+
+  </div>
+</div> 
+ </div>
+
+
+
+<center> 
+<br><br><br>
+<button type="button" class="btn btn-primary btn-lg">
+	
+
+  <a href="cadastro.php"><font color="black"> Cadastre-se </font></button></a>
+
+</center>
+<br><br><br>
+<div class="container">
+  <div class="row">
+   
+    <div class="col"><?php require_once 'slide1.php';?></div>
+    
+  </div>
+</div>
+</center>
+<br><br><br><br><br><br>
+<?php require_once 'rodape.php'; ?>
+
---
+++ tcc com login/simulados-master/login-php_compluido/login-php/login-php/verifica_login.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+if(!$_SESSION['usuario']) {
+	header('Location: index1.php');
+	exit();
+}
