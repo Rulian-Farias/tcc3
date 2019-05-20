c36af113d72ea3348abaf2c85b7a7aefbebe82b6
Rulian Carlo Farias <36968039+Rulian-Farias@users.noreply.github.com>
2019-05-20 14:24:50 -0300

    Add files via upload

--- tcc com login/simulados-master/index.php
+++ tcc com login/simulados-master/index.php
@@ -10,20 +10,21 @@
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script type="text/javascript">
 
+
+	
+</script>
 
 	<title></title>
 </head>
-<body>
+<body >
+	
   
 
 	<?php require_once 'menu.php'; ?>
   <img src="img/iii.png" width="100%" height="800px">
   
-<body background="black">
-
-	
-</body>
  
 
 
@@ -32,27 +33,47 @@
   <div class="row">
 
 
-<div class="col-sm">
-   <img src="img/noticias.png" width="50%"><font size="5" color=" red"> Notícias</font>
+<!-- <div class="col-sm">
+   <img src="img/not.png" width="50%"><font size="5" color=" #ff9933"> Notícias</font>
 </a>
+    </div> -->
+
+    <div class="col-sm">
+
+   <div class="card" style="width: 18rem;">
+  <img src="img/noticia1.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Enem 2019: prazo para pagar boleto da inscrição encerra nesta quinta-feira</p>
+    <a href="https://g1.globo.com/educacao/enem/2019/noticia/2019/05/20/enem-2019-prazo-para-pagar-a-inscricao-encerra-na-quinta-feira.ghtml" class="btn btn-primary">Ver notícia</a>
+  </div>
+</div>
+
     </div>
 
     <div class="col-sm">
-   <a href="https://glo.bo/2WaBhuW"> <img src="img/aluno_enem.jpg" width="30%" > 
-    Candidato que não conseguiu isenção na taxa do Enem 2019 pode entrar 
-    com recurso até esta sexta.
-   </a>
-    </div>
 
-    <div class="col-sm">
-      <a href="https://bit.ly/2vpmfp7"> <img src="img/aluno_enem.jpg" width="30%" >
-    Estudante com isenção negada no Enem ainda pode entrar com recurso.
-</a>
+      <div class="card" style="width: 18rem;">
+  <img src="img/noticia1.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Ser treineiro do Enem é vantajoso</p>
+    <a href="http://correio.rac.com.br/_conteudo/2019/05/campinas_e_rmc/670474-ser-treineiro-do-enem-e-vantajoso.html" class="btn btn-primary">Ver notícia</a>
+  </div>
+</div>
+
     </div>
     <div class="col-sm">
-  <a href="https://enem2019.pro/"><img src="img/aluno_enem.jpg" width="30%" >  
-    Apostila Enem 2019
-</a>
+
+ <div class="card" style="width: 18rem;">
+  <img src="img/noticia1.jpg " class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
     </div>
   </div>
 </div>
@@ -66,9 +87,17 @@
 <center> 
 <br><br><br><br>
 
+<div class="card" style="width: 18rem;">
+  <img src="img/enem.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
 	
 
-  <a href="cadastro.php"><font size="9" color="green"> Cadastre-se </font></button></a>
+  <a href="cadastro.php"><font size="9" color="#ff9900"> Cadastre-se </font></button></a>
 
 </center>
 <br><br>
@@ -81,5 +110,7 @@
 </div>
 </center>
 <br><br><br><br><br><br>
+
 <?php require_once 'rodape.php'; ?>
 
+</body>
